6fd83089e03011e1513df374b46967ebf79d51d3
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:11:48 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caddyserver/caddy/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${version} ]] && exit 0
+set -e
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caddyserver/caddy/releases/latest" | jq -re .tag_name)
 json=$(cat VERSION.json)
 jq --sort-keys \
-    --arg version "${version}" \
-    '.version = $version' <<< "${json}" > VERSION.json
+    --arg version "${version//v/}" \
+    '.version = $version' <<< "${json}" | tee VERSION.json
